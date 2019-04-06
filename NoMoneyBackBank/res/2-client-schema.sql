3b98d044fb404d9b09a5b763a1f9eae7f4134afa
Gráf Tamás <graf.tamas.pcs@mail.com>
2019-04-06 13:50:29 +0200

    Client ddl written

--- NoMoneyBackBank/res/2-client-schema.sql
+++ NoMoneyBackBank/res/2-client-schema.sql
@@ -1,3 +1,20 @@
-CREATE USER nomoneybackbank IDENTIFIED BY admin;
-
-GRANT ALL PRIVILEGES TO nomoneybackbank;
+CREATE TABLE client (
+    client_id              NUMBER(20) NOT NULL,
+    last_name              VARCHAR(50) NOT NULL,
+    firstname              VARCHAR(50) NOT NULL,
+    email                  VARCHAR(50) NOT NULL,
+    password               VARCHAR(50) NOT NULL,
+    date_of_birth          DATE,
+    place_of_birth         VARCHAR(50),
+    mothers_name           VARCHAR(50) NOT NULL,
+    identity_card_number   VARCHAR(50) NOT NULL,
+    address                VARCHAR(50),
+    status                 CHAR(1) NOT NULL,
+    created_at             TIMESTAMP NOT NULL,
+    updated_at             TIMESTAMP NOT NULL,
+    CONSTRAINT client_pk PRIMARY KEY ( client_id ),
+    CONSTRAINT client_ck1 CHECK ( status IN (
+        'A',
+        'D'
+    ) )
+);
