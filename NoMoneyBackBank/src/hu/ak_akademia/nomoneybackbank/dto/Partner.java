d05ffd0603b1dc5eae78d1ba6f001754cc1efb83
Lovászi Tibor <lovaszi.tibor.1980@gmail.com>
2019-04-06 13:24:39 +0200

    partner.java update

--- NoMoneyBackBank/src/hu/ak_akademia/nomoneybackbank/dto/Partner.java
+++ NoMoneyBackBank/src/hu/ak_akademia/nomoneybackbank/dto/Partner.java
@@ -1,9 +1,172 @@
 package hu.ak_akademia.nomoneybackbank.dto;
 
-public class Partner {
+import java.time.LocalDateTime;
 
-	public Partner() {
-		// TODO Auto-generated constructor stub
+public class Partner {
+	
+	private int partnerId;
+	private String partnerName;
+	private String accountNumber;
+	private String notice;
+	private String recipientName;
+	private char status;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	private int clientId;
+	
+	Partner(int partnerId, String partnerName, String accountNumber, String notice, String recipientName, char status, LocalDateTime createdAt, LocalDateTime updatedAt, int clientId) {
+		this.partnerId = partnerId;
+		this.partnerName = partnerName;
+		this.accountNumber = accountNumber;
+		this.notice = notice;
+		this.recipientName = recipientName;
+		this.status = status;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		this.clientId = clientId;
 	}
+
+	public int getPartnerId() {
+		return partnerId;
+	}
+
+	public void setPartnerId(int partnerId) {
+		this.partnerId = partnerId;
+	}
+
+	public String getPartnerName() {
+		return partnerName;
+	}
+
+	public void setPartnerName(String partnerName) {
+		this.partnerName = partnerName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getNotice() {
+		return notice;
+	}
+
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+
+	public String getRecipientName() {
+		return recipientName;
+	}
+
+	public void setRecipientName(String recipientName) {
+		this.recipientName = recipientName;
+	}
+
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((accountNumber == null) ? 0 : accountNumber.hashCode());
+		result = prime * result + clientId;
+		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + ((notice == null) ? 0 : notice.hashCode());
+		result = prime * result + partnerId;
+		result = prime * result + ((partnerName == null) ? 0 : partnerName.hashCode());
+		result = prime * result + ((recipientName == null) ? 0 : recipientName.hashCode());
+		result = prime * result + status;
+		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Partner other = (Partner) obj;
+		if (accountNumber == null) {
+			if (other.accountNumber != null)
+				return false;
+		} else if (!accountNumber.equals(other.accountNumber))
+			return false;
+		if (clientId != other.clientId)
+			return false;
+		if (createdAt == null) {
+			if (other.createdAt != null)
+				return false;
+		} else if (!createdAt.equals(other.createdAt))
+			return false;
+		if (notice == null) {
+			if (other.notice != null)
+				return false;
+		} else if (!notice.equals(other.notice))
+			return false;
+		if (partnerId != other.partnerId)
+			return false;
+		if (partnerName == null) {
+			if (other.partnerName != null)
+				return false;
+		} else if (!partnerName.equals(other.partnerName))
+			return false;
+		if (recipientName == null) {
+			if (other.recipientName != null)
+				return false;
+		} else if (!recipientName.equals(other.recipientName))
+			return false;
+		if (status != other.status)
+			return false;
+		if (updatedAt == null) {
+			if (other.updatedAt != null)
+				return false;
+		} else if (!updatedAt.equals(other.updatedAt))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Partner [partnerId=" + partnerId + ", partnerName=" + partnerName + ", accountNumber=" + accountNumber + ", notice=" + notice + ", recipientName=" + recipientName + ", status="
+				+ status + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + ", clientId=" + clientId + "]";
+	}
+	
 
 }
