68942bfb1e2b899b226a717d09f22c89eda22a07
Lovászi Tibor <lovaszi.tibor.1980@gmail.com>
2019-04-13 11:56:21 +0200

    up

--- NoMoneyBackBank/src/hu/ak_akademia/nomoneybackbank/dto/AbstractDatabaseDao.java
+++ NoMoneyBackBank/src/hu/ak_akademia/nomoneybackbank/dto/AbstractDatabaseDao.java
@@ -6,12 +6,10 @@ import java.sql.DriverManager;
 public abstract class AbstractDatabaseDao<E> implements DatabaseDao<E> {
 
 	@Override
-	public void getConnection() {
-		try (Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "nomoneybackbank", "admin");) {
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			}
+	public Connection getConnection() throws Exception {
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "nomoneybackbank", "admin"); {
+		return connection;
+		}
 	}
 	
 }
