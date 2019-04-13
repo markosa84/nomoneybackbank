855de231416a97200b7422a588c919af52a568c7
Gráf Tamás <graf.tamas.pcs@mail.com>
2019-04-13 11:55:29 +0200

    added more clients

--- NoMoneyBackBank/res/10-client_data.sql
+++ NoMoneyBackBank/res/10-client_data.sql
@@ -117,3 +117,123 @@ INSERT INTO client (
     '963741AE',
     'Baja Váci utca 528'
 );
+
+INSERT INTO client (
+    client_id,
+    last_name,
+    first_name,
+    email,
+    password,
+    date_of_birth,
+    place_of_birth,
+    mothers_name,
+    identity_card_number,
+    address
+) VALUES (
+    client_seq.NEXTVAL,
+    'Minta',
+    'Ember',
+    'info@index.hu',
+    'admin',
+    TO_DATE('2000/01/22', 'YYYY/MM/DD'),
+    'Berlin',
+    'Minta Mama',
+    '551151BA',
+    '2000 Szentendre Fő u. 2'
+);
+
+INSERT INTO client (
+    client_id,
+    last_name,
+    first_name,
+    email,
+    password,
+    date_of_birth,
+    place_of_birth,
+    mothers_name,
+    identity_card_number,
+    address
+) VALUES (
+    client_seq.NEXTVAL,
+    'Minta',
+    'József',
+    'jozef@index.hu',
+    'admin',
+    TO_DATE('1999/05/20', 'YYYY/MM/DD'),
+    'Budapest',
+    'Mamma Mia',
+    '456732BA',
+    '2016 Leányfalu Víz u. 8'
+);
+
+INSERT INTO client (
+    client_id,
+    last_name,
+    first_name,
+    email,
+    password,
+    date_of_birth,
+    place_of_birth,
+    mothers_name,
+    identity_card_number,
+    address
+) VALUES (
+    client_seq.NEXTVAL,
+    'Kiss',
+    'Péter',
+    'kiss@gmail.hu',
+    'admin',
+    TO_DATE('1995/12/22', 'YYYY/MM/DD'),
+    'Warshow',
+    'Nagy Angéla',
+    '752742BA',
+    '2081 Piliscsaba Attila u. 12'
+);
+
+INSERT INTO client (
+    client_id,
+    last_name,
+    first_name,
+    email,
+    password,
+    date_of_birth,
+    place_of_birth,
+    mothers_name,
+    identity_card_number,
+    address
+) VALUES (
+    client_seq.NEXTVAL,
+    'Nagy',
+    'Róbert',
+    'nagyr@gmail.com',
+    'admin',
+    TO_DATE('1985/12/12', 'YYYY/MM/DD'),
+    'Roma',
+    'Tóth Katalin',
+    '271635BA',
+    '2084 Csobánka Tűz u. 8'
+);
+
+INSERT INTO client (
+    client_id,
+    last_name,
+    first_name,
+    email,
+    password,
+    date_of_birth,
+    place_of_birth,
+    mothers_name,
+    identity_card_number,
+    address
+) VALUES (
+    client_seq.NEXTVAL,
+    'Nagy',
+    'Ember',
+    'nagye@gmail.com',
+    'admin',
+    TO_DATE('2002/08/22', 'YYYY/MM/DD'),
+    'Wien',
+    'Kiss Virág',
+    '5383626BA',
+    '2020 Pilisszántó Hegy u. 21'
+);
